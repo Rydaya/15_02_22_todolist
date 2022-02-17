f7f82de52228be5536da223e929af5f39d13cc5c
Kseniia <rydaya97@gmail.com>
2022-02-18 01:26:26 +0200

    create array of tasks and render tasks in DOM

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
+const Task = ({ value }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>{value}</label>
+      <input type="checkbox" id="completedTask" />
+      <button type='submit'>Edit</button>
+      <button type='submit'>Delete</button>
     </div>
-  );
+  )
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newTask: '',
+      tasks: [],
+      taskIndex: 0,
+    }
+  }
+
+  createTask = () => {
+    this.state.tasks.push(this.state.newTask);
+    this.setState({ index: this.state.index + 1 });
+  }
+
+  onChange = (e) => {
+    this.setState({ newTask: e.target.value });
+  }
+
+  clearInput = () => {
+    this.setState({ newTask: '' });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>ToDo List</h1>
+        <form action='#'>
+          <input type='text' />
+          <div>
+            <input type="radio" id="activeTasks" name="filterTasks" value="activeTasks"></input>
+            <label htmlFor="activeTasks">active</label>
+            <input type="radio" id="completedTasks" name="filterTasks" value="completedTasks"></input>
+            <label htmlFor="completedTasks">completed</label>
+            <input type="radio" id="allTasks" name="filterTasks" value="allTasks"></input>
+            <label htmlFor="allTasks">all</label>
+          </div>
+
+          {this.state.tasks.length === 0
+            ? null
+            : this.state.tasks.map((task) => (<Task key={task} value={task} />))
+          }
+          {/* пока key и value равны, не знаю откуда еще брать key */}
+          <div>
+            <input type='text' value={this.state.newTask} onChange={this.onChange} />
+            <button type='submit' onClick={this.createTask}>Submit</button>
+            <button type='submit' onClick={this.clearInput}>Reset</button>
+          </div>
+        </form>
+      </>
+    )
+  }
 }
 
 export default App;
+
